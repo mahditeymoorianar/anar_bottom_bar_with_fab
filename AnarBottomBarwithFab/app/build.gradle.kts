e6fca9fb9261502dbbdc1e3bb95fa0940e20970f
mahditeymoorianar <mahditeymoorianar@gmail.com>
2025-04-19 10:09:56 +0330

    what to write?

--- AnarBottomBarwithFab/app/build.gradle.kts
+++ AnarBottomBarwithFab/app/build.gradle.kts
@@ -1,35 +1,20 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
-}
-
-group = "com.github.mahditeymoorianar"
-version = "1.0.2" // This will match your GitHub tag later
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.github.teymoorianar"
-            artifactId = "anarfabbottombar"
-            version = "1.0.4"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
 }
 
 android {
-    namespace = "com.teymoorianar.anarfabbottombar"
+    namespace = "com.teymoorianar.anarbottombarwithfab"
     compileSdk = 35
 
     defaultConfig {
+        applicationId = "com.teymoorianar.anarbottombarwithfab"
         minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -55,6 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(project(":anarfabbottombar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
